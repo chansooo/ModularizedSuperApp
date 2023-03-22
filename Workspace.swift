66ac43c13767b67d8107c57feada8491f1a5099b
chansooo <kcs05008@gmail.com>
2023-03-22 10:47:52 +0900

    workspace 생성

---
+++ Workspace.swift
@@ -0,0 +1,15 @@
+//
+//  Workspace.swift
+//  Config
+//
+//  Created by 김찬수 on 2023/03/21.
+//
+
+import ProjectDescription
+
+let appName = "ModularizedSuperApp"
+
+let workspace = Workspace(
+    name: appName,
+    projects: ["Projects/**"]
+)
