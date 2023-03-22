eb5134757dd2543075b4bf2c5c28fbb85f251e13
chansooo <kcs05008@gmail.com>
2023-03-22 10:50:42 +0900

    기본적인 Target 생성 함수 구현

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,1 +1,124 @@
 import ProjectDescription
+
+extension Project {
+    private static let organizationName = "chansoo.io"
+    
+    public static func app(name: String,
+                           platform: Platform,
+                           iOSTargetVersion: String,
+                           infoPlist: String,
+                           dependencies: [TargetDependency] = []) -> Project {
+        let targets = makeAppTargets(name: name,
+                                     platform: platform,
+                                     iOSTargetVersion: iOSTargetVersion,
+                                     infoPlist: infoPlist,
+                                     dependencies: dependencies)
+        return Project(name: name,
+                       organizationName: organizationName,
+                       targets: targets)
+    }
+    
+    public static func frameworkWithDemoApp(name: String,
+                                            platform: Platform,
+                                            iOSTargetVersion: String,
+                                            infoPlist: [String: InfoPlist.Value] = [:],
+                                            dependencies: [TargetDependency] = []) -> Project {
+        var targets = makeFrameworkTargets(name: name,
+                                           platform: platform,
+                                           iOSTargetVersion: iOSTargetVersion,
+                                           dependencies: dependencies)
+        targets.append(contentsOf: makeAppTargets(name: "\(name)DemoApp",
+                                                  platform: platform,
+                                                  iOSTargetVersion: iOSTargetVersion,
+                                                  infoPlist: infoPlist,
+                                                  dependencies: [.target(name: name)]))
+        
+        return Project(name: name,
+                       organizationName: organizationName,
+                       targets: targets)
+    }
+    
+    public static func framework(name: String,
+                                 platform: Platform, iOSTargetVersion: String,
+                                 dependencies: [TargetDependency] = []) -> Project {
+        let targets = makeFrameworkTargets(name: name,
+                                           platform: platform,
+                                           iOSTargetVersion: iOSTargetVersion,
+                                           dependencies: dependencies)
+        return Project(name: name,
+                       organizationName: organizationName,
+                       targets: targets)
+    }
+}
+
+private extension Project {
+
+    
+    static func makeFrameworkTargets(name: String, platform: Platform, iOSTargetVersion: String, dependencies: [TargetDependency] = []) -> [Target] {
+        let sources = Target(name: name,
+                             platform: platform,
+                             product: .framework,
+                             bundleId: "team.io.\(name)",
+                             deploymentTarget: .iOS(targetVersion: iOSTargetVersion, devices: [.iphone]),
+                             infoPlist: .default,
+                             sources: ["Sources/**"],
+                             resources: ["Resources/**"],
+                             dependencies: dependencies)
+//        let tests = Target(name: "\(name)Tests",
+//                           platform: platform,
+//                           product: .unitTests,
+//                           bundleId: "team.io.\(name)Tests",
+//                           infoPlist: .default,
+//                           sources: ["Tests/**"],
+//                           resources: [],
+//                           dependencies: [
+//                            .target(name: name)
+//                           ])
+        return [sources]
+    }
+    
+    static func makeAppTargets(name: String, platform: Platform, iOSTargetVersion: String, infoPlist: [String: InfoPlist.Value] = [:], dependencies: [TargetDependency] = []) -> [Target] {
+        let platform: Platform = platform
+        
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .app,
+            bundleId: "team.io.\(name)",
+            deploymentTarget: .iOS(targetVersion: iOSTargetVersion, devices: [.iphone]),
+            infoPlist: .extendingDefault(with: infoPlist),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: dependencies
+        )
+        
+//        let testTarget = Target(
+//            name: "\(name)Tests",
+//            platform: platform,
+//            product: .unitTests,
+//            bundleId: "team.io.Tests",
+//            infoPlist: .default,
+//            sources: ["Tests/**"],
+//            dependencies: [
+//                .target(name: "\(name)"),
+//            ])
+        return [mainTarget]
+    }
+    
+    static func makeAppTargets(name: String, platform: Platform, iOSTargetVersion: String, infoPlist: String, dependencies: [TargetDependency] = []) -> [Target] {
+        let platform: Platform = platform
+        
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .app,
+            bundleId: "team.io.\(name)",
+            deploymentTarget: .iOS(targetVersion: iOSTargetVersion, devices: [.iphone]),
+            infoPlist: InfoPlist(stringLiteral: infoPlist),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: dependencies
+        )
+        return [mainTarget]
+    }
+}
