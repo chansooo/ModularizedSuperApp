ca5b9c012ca18d88fd9dc28844c226900845e6a8
chansooo <kcs05008@gmail.com>
2023-03-22 10:51:20 +0900

    하나의 프로젝트에 Interface, Implement로 Target 분리해서 프로젝트 생성하는 메소드 구현

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -49,9 +49,122 @@ extension Project {
                        organizationName: organizationName,
                        targets: targets)
     }
+        
+    /// 현재 경로 내부의 Implement, Interface 두개의 디렉토리에 각각 Target을 가지는 Project를 만듭니다.
+    /// interface와 implement에 필요한 dependency를 각각 주입해줍니다.
+    /// implement는 자동으로 interface에 대한 종속성을 가지고 있습니다.
+    public static func inversedLibrary(
+        name: String,
+        platform: Platform,
+        iOSTargetVersion: String,
+        interfaceDependencies: [TargetDependency] = [],
+        implementDependencies: [TargetDependency] = []
+    ) -> Project {
+
+        let interfaceTarget = makeInterfaceStaticLibraryTarget(
+            name: name,
+            platform: platform,
+            iOSTargetVersion: iOSTargetVersion,
+            dependencies: interfaceDependencies
+        )
+        
+        let implementTarget = makeImplementStaticLibraryTarget(
+            name: name,
+            platform: platform,
+            iOSTargetVersion: iOSTargetVersion,
+            dependencies: implementDependencies + [.target(name: name)]
+        )
+        
+        return Project(name: name,
+                       organizationName: organizationName,
+                       targets: [interfaceTarget, implementTarget])
+    }
 }
 
 private extension Project {
+    /// Resource 폴더 내부에 Interface 폴더는 프로토콜만 존재
+    /// Implement 폴더 내부에 프로토콜을 실제 구현한 부분이 존재.
+    /// Inject 해주는 부분만 뭐시기Impl를 import하면 됨.
+    static func makeNestedFrameworkTargets(name: String, platform: Platform, iOSTargetVersion: String, dependencies: [TargetDependency] = []) -> [Target] {
+        
+        
+        let interfaceSources = Target(name: name,
+                             platform: platform,
+                             product: .staticLibrary,
+                             bundleId: "team.io.\(name)",
+                             deploymentTarget: .iOS(
+                                targetVersion: iOSTargetVersion,
+                                devices: [.iphone]
+                             ),
+                             infoPlist: .default,
+                             sources: ["Sources/Interface/**"],
+//                             resources: ["Resources/**"],
+                             dependencies: dependencies)
+        
+        let ImplementSource = Target(name: "\(name)Impl",
+                              platform: platform,
+                              product: .staticLibrary,
+                              bundleId: "team.io.\(name)",
+                              deploymentTarget: .iOS(
+                                targetVersion: iOSTargetVersion,
+                                devices: [.iphone]
+                              ),
+                              infoPlist: .default,
+                              sources: ["Sources/Implement/**"],
+ //                             resources: ["Resources/**"],
+                              dependencies: dependencies)
+//        let tests = Target(name: "\(name)Tests",
+//                           platform: platform,
+//                           product: .unitTests,
+//                           bundleId: "team.io.\(name)Tests",
+//                           infoPlist: .default,
+//                           sources: ["Tests/**"],
+//                           resources: [],
+//                           dependencies: [
+//                            .target(name: name)
+//                           ])
+        return [interfaceSources, ImplementSource]
+    }
+    
+    static func makeImplementStaticLibraryTarget(
+        name: String,
+        platform: Platform,
+        iOSTargetVersion: String,
+        dependencies: [TargetDependency] = []
+    ) -> Target {
+        return Target(name: "\(name)Impl",
+                      platform: platform,
+                      product: .staticLibrary,
+                      bundleId: "team.io.\(name)",
+                      deploymentTarget: .iOS(
+                        targetVersion: iOSTargetVersion,
+                        devices: [.iphone]
+                      ),
+                      infoPlist: .default,
+                      sources: ["Sources/Implement/**"],
+//                      resources: ["Resources/**"],
+                      dependencies: dependencies)
+    }
+    
+    static func makeInterfaceStaticLibraryTarget(
+        name: String,
+        platform: Platform,
+        iOSTargetVersion: String,
+        dependencies: [TargetDependency] = []
+    ) -> Target {
+        return Target(name: name,
+                             platform: platform,
+                             product: .staticLibrary,
+                             bundleId: "team.io.\(name)",
+                             deploymentTarget: .iOS(
+                                targetVersion: iOSTargetVersion,
+                                devices: [.iphone]
+                             ),
+                             infoPlist: .default,
+                             sources: ["Sources/Interface/**"],
+//                             resources: ["Resources/**"],
+                             dependencies: dependencies)
+    }
 
     
     static func makeFrameworkTargets(name: String, platform: Platform, iOSTargetVersion: String, dependencies: [TargetDependency] = []) -> [Target] {
