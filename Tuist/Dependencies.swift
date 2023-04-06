f1f8b482d43456bca6480b3ec564a1bebeab7f2c
chansooo <kcs05008@gmail.com>
2023-04-06 13:49:15 +0900

    dependency 프레임워크로 설정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -7,14 +7,24 @@
 
 import ProjectDescription
 
-let spm = SwiftPackageManagerDependencies([
-    .remote(url: "https://github.com/ReactiveX/RxSwift", requirement: .upToNextMajor(from: "6.0.0")),
-    
-//    .remote(url: "https://github.com/RxSwiftCommunity/RxGesture", requirement: .upToNextMinor(from: "4.0.0")),
-//    .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.0")),
-//    .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0")),
-    .remote(url: "https://github.com/uber/RIBs", requirement: .upToNextMajor(from: "0.9"))
-])
+let spm = SwiftPackageManagerDependencies(
+    [
+        .remote(url: "https://github.com/ReactiveX/RxSwift", requirement: .upToNextMajor(from: "6.5.0")),
+        .remote(url: "https://github.com/uber/RIBs", requirement: .upToNextMajor(from: "0.13.0"))
+        //    .remote(url: "https://github.com/RxSwiftCommunity/RxGesture", requirement: .upToNextMinor(from: "4.0.0")),
+        //    .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.0")),
+        //    .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0")),
+    ],
+    productTypes: [
+        "RIBs": .framework,
+        "RxSwift": .framework,
+        "RxCocoa": .framework,
+        "RxRelay": .framework,
+//        "RxGesture": .framework,
+//        "SnapKit": .framework,
+//        "Kingfisher": .framework
+    ]
+)
 
 let dependencies = Dependencies(
     swiftPackageManager: spm,
